c0f97dbbd0c2dbae0237615d3ea29e21227407d7
SamuSeen <elooykinge@gmail.com>
2021-11-07 18:51:19 +0100

    Created Project

---
+++ account.java
@@ -0,0 +1,8 @@
+public class account {
+
+    @Override
+    public String toString() {
+        return "account []";
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+
+    @Override
+    public String toString() {
+        return "main []";
+    }
+
+}
